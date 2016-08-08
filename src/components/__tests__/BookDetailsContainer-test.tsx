224c3a344115e228f69f9dae6df22eb17ce8637b
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-08-08 14:34:06 -0400

    added BookDetailsContainer test for Lanes rendering

--- src/components/__tests__/BookDetailsContainer-test.tsx
+++ src/components/__tests__/BookDetailsContainer-test.tsx
@@ -6,6 +6,7 @@ import { shallow } from "enzyme";
 
 import BookDetailsContainer from "../BookDetailsContainer";
 import BookDetails from "../BookDetails";
+import Lanes from "opds-web-client/lib/components/Lanes";
 import { buildCollectionStore } from "opds-web-client/lib/store";
 
 let book = {
@@ -17,7 +18,14 @@ let book = {
   summary: "&lt;b&gt;Sam and Remi Fargo race for treasure&#8212;and survival&#8212;in this lightning-paced new adventure from #1&lt;i&gt; New York Times&lt;/i&gt; bestselling author Clive Cussler.&lt;/b&gt;&lt;br /&gt;&lt;br /&gt;Husband-and-wife team Sam and Remi Fargo are in Mexico when they come upon a remarkable discovery&#8212;the mummified remainsof a man clutching an ancient sealed pot. Within the pot is a Mayan book larger than any known before.&lt;br /&gt;&lt;br /&gt;The book contains astonishing information about the Mayans, their cities, and about mankind itself. The secrets are so powerful that some people would do anything to possess them&#8212;as the Fargos are about to find out. Many men and women are going to die for that book.",
   imageUrl: "https://dlotdqc6pnwqb.cloudfront.net/3M/crrmnr9/cover.jpg",
   borrowUrl: "borrow url",
-  raw: {}
+  raw: {
+    link: [{
+      $: {
+        rel: { value: "related" },
+        href: { value: "related url" }
+      }
+    }]
+  }
 };
 
 class DefaultBookDetails extends React.Component<any, any> {
@@ -27,11 +35,19 @@ class DefaultBookDetails extends React.Component<any, any> {
 }
 
 describe("BookDetailsContainer", () => {
-  it("renders BookDetails with its child's props", () => {
-    let context = { recommendationsStore: buildCollectionStore() };
-    let borrowBook = url => Promise.resolve();
-    let fulfillBook = url => Promise.resolve();
-    let wrapper = shallow(
+  let wrapper;
+  let store;
+  let context;
+  let borrowBook = url => Promise.resolve();
+  let fulfillBook = url => Promise.resolve();
+
+  beforeEach(() => {
+    store = buildCollectionStore();
+    context = {
+      recommendationsStore: store,
+      proxyUrl: "proxy url"
+    };
+    wrapper = shallow(
       <BookDetailsContainer
         book={book}
         bookUrl="book url"
@@ -47,11 +63,22 @@ describe("BookDetailsContainer", () => {
       </BookDetailsContainer>,
       { context }
     );
+  });
 
+  it("renders BookDetails with its child's props", () => {
     let bookDetails = wrapper.find(BookDetails);
     expect(bookDetails.prop("book")).to.equal(book);
     expect(bookDetails.prop("borrowBook")).to.equal(borrowBook);
     expect(bookDetails.prop("fulfillBook")).to.equal(fulfillBook);
     expect(bookDetails.prop("isSignedIn")).to.equal(true);
+  });
+
+  it("renders related lanes", () => {
+    let lanes = wrapper.find(Lanes);
+    expect(lanes.prop("url")).to.equal("related url");
+    expect(lanes.prop("proxyUrl")).to.equal("proxy url");
+    expect(lanes.prop("store")).to.equal(store);
+    expect(lanes.prop("hideMoreLinks")).to.equal(true);
+    expect(lanes.prop("hiddenBookIds")).to.deep.equal([book.id]);
   });
 });
