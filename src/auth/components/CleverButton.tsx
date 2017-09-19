e5ba4c564f704b7244566b2561e4df81e2f65aea
Amy Slagle <amyslagle@nypl.org>
2017-09-19 12:33:02 -0400

    Updated Clever button for new auth document format.

--- src/auth/components/CleverButton.tsx
+++ src/auth/components/CleverButton.tsx
@@ -2,18 +2,29 @@ import * as React from "react";
 import { AuthProvider, AuthMethod } from "opds-web-client/lib/interfaces";
 import { AuthButtonProps } from "opds-web-client/lib/components/AuthProviderSelectionForm";
 
+export interface AuthLink {
+  rel: string;
+  href: string;
+}
+
 export interface CleverAuthMethod extends AuthMethod {
-  links: {
-    authenticate: URL;
-  };
+  links?: AuthLink[];
 }
 
 export default class CleverButton extends React.Component<AuthButtonProps<CleverAuthMethod>, any> {
   render() {
     let currentUrl = window.location.origin + window.location.pathname;
-    let authUrl = this.props.provider.method.links.authenticate + "&redirect_uri=" + encodeURIComponent(encodeURIComponent(currentUrl));
+    let authUrl;
+    for (const link of this.props.provider.method.links || []) {
+      if (link.rel === "authenticate") {
+        authUrl = link.href + "&redirect_uri=" + encodeURIComponent(encodeURIComponent(currentUrl));
+        break;
+      }
+    }
     return (
-      <a href={authUrl} className="clever-button" aria-label="log in with clever" />
+      authUrl ?
+      <a href={authUrl} className="clever-button" aria-label="log in with clever" /> :
+      null
     );
   }
 }
