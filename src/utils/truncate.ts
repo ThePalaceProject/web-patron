47da7823c45924666e8c55e3d6a5bace160c4c94
kristojorg <kristo.jorgenson@gmail.com>
2020-04-15 19:07:40 -0600

    add truncate string tests

--- src/utils/truncate.ts
+++ src/utils/truncate.ts
@@ -8,11 +8,17 @@ const truncateString = (
   length: number,
   useWordBoundary = true
 ) => {
+  // if the string is empty, just return it
+  if (str === "") return str;
+
   if (str.length < length) return str;
-  const substring = str.substring(0, length - 1);
+  const substring = str.substring(0, length);
   const finalTruncation = useWordBoundary
     ? substring.substring(0, substring.lastIndexOf(" "))
     : substring;
+
+  // ignore word boundary setting if the string ends up empty
+  if (finalTruncation === "") return truncateString(str, length, false);
 
   return finalTruncation + "...";
 };
