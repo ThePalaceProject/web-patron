5bab4198f05ce8e22d129c6bc687fe2c5c408a38
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2020-06-09 17:42:12 +0200

    update icon fill

--- src/icons/ArrowForward.tsx
+++ src/icons/ArrowForward.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 
 const SvgArrowForward = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg height="1em" viewBox="0 0 24 24" width="1em" {...props}>
+  <svg
+    height="1em"
+    viewBox="0 0 24 24"
+    width="1em"
+    fill="currentColor"
+    {...props}
+  >
     <path d="M0 0h24v24H0z" fill="none" />
     <path d="M5.88 4.12L13.76 12l-7.88 7.88L8 22l10-10L8 2z" />
   </svg>
