01bfe245f50b85e87b2abafdd3c48624b5c57861
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2020-06-10 15:25:06 +0200

    update pageTitle

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -2,9 +2,26 @@
 import { jsx } from "theme-ui";
 import * as React from "react";
 import { H1 } from "./Text";
+import Stack from "./Stack";
+import Select from "./Select";
 
 const PageTitle: React.FC = ({ children }) => {
-  return <H1 sx={{ px: 5, my: 4 }}>{children}</H1>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        px: 5,
+        my: 4,
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}
+    >
+      <H1>{children}</H1>
+      <Select>
+        <option>hi</option>
+      </Select>
+    </Stack>
+  );
 };
 
 export default PageTitle;
