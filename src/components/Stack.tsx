327496e4f0f2562d2dea01ef49349d6597a7cd80
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2020-06-11 13:52:31 +0200

    stack fix

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, css } from "theme-ui";
+import { jsx } from "theme-ui";
 import * as React from "react";
 
 type StackProps = {
@@ -15,24 +15,20 @@ const Stack: React.FC<StackProps> = ({
   const numChildren = React.Children.count(children);
 
   return (
-    <div sx={{ display: "flex" }} {...rest}>
+    <div
+      sx={{
+        display: "flex"
+      }}
+      {...rest}
+    >
       {React.Children.map(children, (child, index) => {
         const isLastChild = index === numChildren - 1;
+        const newSpacing = {
+          mr: !isLastChild && direction === "row" ? spacing : undefined,
+          mb: !isLastChild && direction === "column" ? spacing : undefined
+        };
         if (!React.isValidElement(child)) return null;
-        const propsWithSpacing =
-          direction === "row"
-            ? {
-                css: css({
-                  mr: isLastChild ? null : spacing
-                }),
-                ...child.props
-              }
-            : {
-                css: css({ mb: isLastChild ? null : spacing }),
-                ...child.props
-              };
-
-        return React.cloneElement(child, propsWithSpacing);
+        return <div sx={newSpacing}>{child}</div>;
       })}
     </div>
   );
