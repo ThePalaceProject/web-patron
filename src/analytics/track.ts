9c7ac13e4d523dc6f820689847cce5dce6cc4668
kristojorg <kristo.jorgenson@gmail.com>
2020-09-09 13:57:17 +0200

    change track event names

--- src/analytics/track.ts
+++ src/analytics/track.ts
@@ -6,7 +6,9 @@ export function updateDataLayer(data: any) {
   window?.dataLayer?.push(data);
 }
 
-function event(name: string, data: any) {
+type AnyEvent = ApplicationEvent | UserEvent;
+
+function event(name: AnyEvent, data: any) {
   window?.dataLayer?.push({
     event: name,
     ...data
@@ -14,18 +16,21 @@ function event(name: string, data: any) {
 }
 
 type UserEvent =
-  | "searched"
-  | "borrowed_book"
-  | "reserved_book"
-  | "fulfilled_book"
+  | "search_performed"
+  | "book_borrowed"
+  | "book_reserved"
+  | "book_fulfilled"
   | "pageview";
 export function userEvent(name: UserEvent, data: any) {
-  event(name, { eventCategory: "User Event", ...data });
+  event(name, { event_category: "User Event", ...data });
 }
 
-type ApplicationEvent = "loaded_collection" | "loaded_book";
+type ApplicationEvent =
+  | "collection_loaded"
+  | "book_loaded"
+  | "performance_metric_recorded";
 export function appEvent(name: ApplicationEvent, data: any) {
-  event(name, { eventCategory: "Application Event", ...data });
+  event(name, { event_category: "Application Event", ...data });
 }
 
 // allows us to track performance using web vitals reports
@@ -36,10 +41,11 @@ export function trackWebVitals({
   value,
   label
 }: NextWebVitalsMetric) {
-  event(name, {
-    eventCategory: "Performance",
-    label: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    metricId: id // id unique to current page load
+  appEvent("performance_metric_recorded", {
+    metric_name: name,
+    metric_category:
+      label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    metric_value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    metric_id: id // id unique to current page load
   });
 }
