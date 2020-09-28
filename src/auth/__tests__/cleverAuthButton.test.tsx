3a7971969e9bf3899a8d80de48a9436a906355c0
kristojorg <kristo.jorgenson@gmail.com>
2020-09-28 17:44:30 +0200

    remove uncapitalized

--- src/auth/__tests__/cleverAuthButton.test.tsx
+++ src/auth/__tests__/cleverAuthButton.test.tsx
@@ -1,42 +1,49 @@
 import * as React from "react";
-import { fixtures, render } from "test-utils";
+import { render } from "test-utils";
 import CleverButton from "../CleverAuthButton";
 
 describe("CleverButton", () => {
-  test("constructs proper authUrl", () => {
-    const utils = render(<CleverButton method={fixtures.cleverAuthMethod} />);
-    const button = utils.getByLabelText("Log In with Clever");
-    expect(button).toHaveAttribute(
-      "href",
-      "https://example.com/oauth_authenticate?provider=Clever&redirect_uri=http%253A%252F%252Ftest-domain.com%252F"
+  let wrapper: any;
+  let provider: any;
+
+  beforeEach(() => {
+    provider = {
+      id: "http://librarysimplified.org/authtype/OAuth-with-intermediary",
+      plugin: {
+        type: "http://librarysimplified.org/authtype/OAuth-with-intermediary",
+        lookForCredentials: jest.fn(),
+        buttonComponent: jest.fn()
+      },
+      method: {
+        type: "http://librarysimplified.org/authtype/OAuth-with-intermediary",
+        description: "Clever",
+        links: [
+          {
+            href:
+              "https://testing.com/USOEI/oauth_authenticate?provider=Clever",
+            rel: "authenticate"
+          }
+        ]
+      }
+    };
+    wrapper = render(<CleverButton provider={provider} />);
+  });
+
+  it("constructs proper authUrl", () => {
+    expect(getAuthUrl(provider, "/")).toBe(
+      "https://testing.com/USOEI/oauth_authenticate?provider=Clever&redirect_uri=%252F"
     );
   });
 
-  test("returns button labeled Log In With Clever", () => {
-    const utils = render(<CleverButton method={fixtures.cleverAuthMethod} />);
-    const button = utils.getByLabelText("Log In with Clever");
-    expect(button).toBeInTheDocument();
-    expect(button).toBeEmptyDOMElement();
+  it("when there is an auth provider returns button labeled Log In With Clever", () => {
+    const buttonLabelText = wrapper.getByLabelText("Log In with Clever");
+    const buttonText = wrapper.getByText("Log In With Clever");
+    expect(buttonLabelText).toBeInTheDocument();
+    expect(buttonText).toBeInTheDocument();
   });
 
-  test("has text content when no image url supplied", () => {
-    const utils = render(
-      <CleverButton
-        method={{
-          ...fixtures.cleverAuthMethod,
-          links: [{ href: "/authenticate", rel: "authenticate" }]
-        }}
-      />
-    );
-    const button = utils.getByLabelText("Log In with Clever");
-    expect(button).toBeInTheDocument();
-    expect(button).toHaveTextContent("Log In With Clever");
-  });
-
-  test("returns null when there is no authenticate link in method", () => {
-    const { container } = render(
-      <CleverButton method={{ ...fixtures.cleverAuthMethod, links: [] }} />
-    );
+  it("when there is no auth provider returns empty element", () => {
+    const { container } = render(<CleverButton provider={undefined} />);
     expect(container.firstChild).toBeNull();
   });
 });
