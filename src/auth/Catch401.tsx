31d223243b707cf519461742c95717840555df4b
kristojorg <kristo.jorgenson@gmail.com>
2020-11-04 16:41:01 +0000

    catch 401s

---
+++ src/auth/Catch401.tsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import * as React from "react";
+import useAuthModalContext from "auth/AuthModalContext";
+import { keyInterface, SWRConfig } from "swr";
+import swrConfig from "utils/swrConfig";
+import { ServerError } from "errors";
+import track from "analytics/track";
+
+const CatchFetchErrors: React.FC = ({ children }) => {
+  const { showModal } = useAuthModalContext();
+
+  function handle401() {
+    console.log("401!");
+    showModal();
+  }
+
+  const config = {
+    ...swrConfig,
+    onError: (error: Error, _key: keyInterface) => {
+      if (error instanceof ServerError) {
+        if (error.info.status === 401) {
+          handle401();
+          return;
+        }
+      }
+      // track the error to bugsnag
+      track.error(error);
+    }
+  };
+
+  return <SWRConfig value={config}>{children}</SWRConfig>;
+};
+
+export default CatchFetchErrors;
