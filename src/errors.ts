9c5e6677965a3202415c5a1d4d3d36726cb6885a
kristojorg <kristo.jorgenson@gmail.com>
2020-11-13 15:38:21 +0000

    errors init

--- src/errors.ts
+++ src/errors.ts
@@ -2,11 +2,13 @@ import { OPDS1 } from "interfaces";
 
 export default class ApplicationError extends Error {
   info: OPDS1.ProblemDocument;
+  baseError?: Error;
 
   constructor(m: string, baseError?: Error) {
-    super(`${m}${baseError ? `\nBase Error:\n${baseError.message}` : ""}`);
+    super(m);
     Object.setPrototypeOf(this, ApplicationError.prototype);
     this.name = "Application Error";
+    this.baseError = baseError;
     this.info = {
       title: "Application Error",
       detail: m
@@ -16,8 +18,9 @@ export default class ApplicationError extends Error {
 
 export class PageNotFoundError extends ApplicationError {
   constructor(m: string, baseError?: Error) {
-    super(`${m}${baseError ? baseError.message : ""}`);
+    super(m);
     Object.setPrototypeOf(this, PageNotFoundError.prototype);
+    this.baseError = baseError;
     this.name = "Page Not Found Error";
     this.info = {
       title: "Page Not Found",
@@ -29,8 +32,9 @@ export class PageNotFoundError extends ApplicationError {
 
 export class UnimplementedError extends ApplicationError {
   constructor(m: string, baseError?: Error) {
-    super(`${m}${baseError ? baseError.message : ""}`);
+    super(m);
     Object.setPrototypeOf(this, UnimplementedError.prototype);
+    this.baseError = baseError;
     this.name = "Unimplemented Error";
     this.info = {
       title: "Unimplemented",
@@ -41,9 +45,10 @@ export class UnimplementedError extends ApplicationError {
 
 export class AppSetupError extends ApplicationError {
   constructor(m: string, baseError?: Error) {
-    super(`${m}${baseError ? baseError.message : ""}`);
+    super(m);
     Object.setPrototypeOf(this, AppSetupError.prototype);
     this.name = "App Setup Error";
+    this.baseError = baseError;
     this.info = {
       status: 500,
       title: "App Setup Error",
@@ -59,14 +64,14 @@ export class FetchError extends ApplicationError {
   url: string;
 
   constructor(url: string, baseError?: Error) {
-    super(`${baseError ? baseError.message : ""}`);
+    super(`Failed to fetch url: ${url}`);
     Object.setPrototypeOf(this, FetchError.prototype);
     this.name = "Fetch Error";
+    this.baseError = baseError;
     this.url = url;
     this.info = {
       title: "Fetch Error",
-      detail:
-        "The fetch promise for the requested resource was rejected. This is probably an offline, CORS, or other network error."
+      detail: `The fetch promise for the requested resource was rejected. This is probably an offline, CORS, or other network error. Requested URL: ${url}`
     };
   }
 }
