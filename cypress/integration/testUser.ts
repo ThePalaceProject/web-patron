25664874c02395f0c2966397fd7dc73fcfbbd58a
kristojorg <kristo.jorgenson@gmail.com>
2021-06-26 23:29:48 +0200

    init cypress tests

---
+++ cypress/integration/testUser.ts
@@ -0,0 +1,10 @@
+const username = "A3IAYP9NDS";
+const password = "9864";
+
+/**
+ * Extends cypress's visit command to add authentication
+ */
+export const visitAuthd = (
+  url: string,
+  options: Partial<Cypress.VisitOptions>
+) => cy.visit(url, { ...options, auth: { username, password } });
