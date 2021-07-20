080e2cd61e4068683b812f4cd7570973f8ea6c84
Ray Lee <ray.lee@lyrasis.org>
2021-07-20 13:26:09 -0400

    Update logo and text in mobile app callouts.

--- src/components/storeBadges/GooglePlayBadge.tsx
+++ src/components/storeBadges/GooglePlayBadge.tsx
@@ -7,7 +7,8 @@ const GooglePlayBadge = (props: React.ComponentProps<"a">) => {
     <a
       rel="noopener noreferrer"
       target="__blank"
-      aria-label="Get SimplyE on the Google Play Store"
+      aria-label="Get Palace on the Google Play Store"
+      // TODO: Replace with correct URL when Palace is in the Play Store.
       href="https://play.google.com/store/apps/details?id=org.nypl.simplified.simplye&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
       sx={{ display: "block" }}
       {...props}
