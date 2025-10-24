fd944e0b7700215447b49f5404a22008931368c5
Tyler Jones <tyler.jones@lyrasis.org>
2025-10-24 07:53:50 -0700

    removed unneeded aria-label

--- src/components/bookDetails/index.tsx
+++ src/components/bookDetails/index.tsx
@@ -135,7 +135,6 @@ const Summary: React.FC<{ book: AnyBook; className?: string }> = ({
 const SimplyECallout: React.FC<{ className?: "string" }> = ({ className }) => {
   return (
     <section
-      // aria-label="Download the Palace Mobile App"
       sx={{
         mt: 4,
         bg: "ui.gray.lightWarm",
