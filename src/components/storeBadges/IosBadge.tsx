c0e2ef8b07466126815a8381468099066ec2becf
Tyler Jones <tyler.jones@lyrasis.org>
2026-08-14 22:17:43 -0700

    add translation keys for GooglePlayBadge and IosBadge

--- src/components/storeBadges/IosBadge.tsx
+++ src/components/storeBadges/IosBadge.tsx
@@ -1,13 +1,18 @@
 import * as React from "react";
 import SvgIosBadge from "icons/IosBadge";
+import { useTranslation } from "next-i18next/pages";
 
 const IosBadge = (props: React.ComponentProps<"a">) => {
+  const { t } = useTranslation();
   return (
     <a
       rel="noopener noreferrer"
       target="__blank"
       href="https://apps.apple.com/us/app/the-palace-project/id1574359693"
-      aria-label="Download Palace on the Apple App Store"
+      aria-label={t(
+        "iosBadge.download",
+        "Download Palace on the Apple App Store"
+      )}
       sx={{ display: "block" }}
       {...props}
     >
